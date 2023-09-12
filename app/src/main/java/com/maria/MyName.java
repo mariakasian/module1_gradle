1d2f2c50513a0c5ab4f29860cbe5ed78e13f520f
Maria Kasian <124454348+mariakasian@users.noreply.github.com>
2023-09-12 16:49:27 +0400

    initial commit

---
+++ app/src/main/java/com/maria/MyName.java
@@ -0,0 +1,14 @@
+package com.maria;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class MyName {
+    public static void main(String[] args) {
+
+        Person person = new Person("Maria", "Kasian");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(person));
+    }
+}
+
